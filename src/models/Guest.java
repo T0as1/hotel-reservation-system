da204ef968bdfc150d75283c7dcc888ec2e9df28
YoussefAlaa1407 <joealaa824@gmail.com>
2026-04-25 19:27:05 +0300

    Implemented showDashboard in Guest

--- src/models/Guest.java
+++ src/models/Guest.java
@@ -1,6 +1,7 @@
 package models;
 
 import data.HotelDatabase;
+import enums.PaymentMethod;
 import enums.ReservationStatus;
 import exceptions.*;
 import enums.Gender;
@@ -19,7 +20,7 @@ public class Guest extends User implements Payable {
     // data fields
 
     private LocalDate dateOfBirth;
-    private double balance;
+    private double balance = 2000.0;
     private String address;
     private Gender gender;
     private String roomPreferences;
@@ -85,7 +86,11 @@ public class Guest extends User implements Payable {
     }
 
     public void setRoomPreferences(String roomPreferences) {
-        this.roomPreferences = roomPreferences;
+        if(roomPreferences == null)
+            this.roomPreferences = "None";
+        else {
+            this.roomPreferences = roomPreferences;
+        }
     }
 
 
@@ -161,8 +166,256 @@ public class Guest extends User implements Payable {
                 + getRoomPreferences();
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if(!(obj instanceof Guest))
+            return false;
+        Guest guest = (Guest) obj;
+        return this.getUsername().equals(guest.getUsername());
+    }
+
     public void showDashboard(Scanner sc){
-        System.out.println("---Guest Dashboard---");
+        boolean loggedIn = true;
+        System.out.println("\n---Guest Dashboard---");
+        while (loggedIn) {
+            System.out.println("""
+                    1.View Available Rooms
+                    2.Make a Reservation
+                    3.View My Reservations
+                    4.Cancel Reservation
+                    5.Check out
+                    6.Logout""");
+            String choice = sc.nextLine();
+            switch (choice){
+                case "1":
+                    System.out.println("\n---Available Rooms List---");
+                    for(Room r: viewAvailableRooms()){
+                        System.out.println(r);
+                    }
+                    System.out.println("\n---------------------------");
+                    break;
+
+                case "2": try{
+                    System.out.println("0.Go back\n" +
+                            "(To view available rooms, choose option 1 from the dashboard)\n" +
+                            "Enter room number you want to book");
+                    int resRoomNo = -1; // invalid value for while loop
+                    while (resRoomNo == -1)
+                    {
+                        try {
+                            resRoomNo = Integer.parseInt(sc.nextLine());
+                        } catch (NumberFormatException e)
+                        {
+                            System.out.println("Please enter a valid room number");
+                        } catch (Exception e){
+                            System.out.println("Unexpected error");
+                        }
+                    }
+                    if(resRoomNo == 0)
+                        break;
+                    Room selectRoom =  HotelDatabase.findRoomByNumber(resRoomNo);
+                    LocalDate inDate = null;
+                    LocalDate outDate = null;
+                    LocalDate today = LocalDate.now();
+                    if(selectRoom != null && selectRoom.isAvailable()){
+                        // This loop is to ensure dates are input correctly, then it is broken and flow continues;
+                        while(true) {
+                            try {
+                                System.out.print("Enter Check-In Date (YYYY-MM-DD): ");
+                                LocalDate temp = LocalDate.parse(sc.nextLine());
+                                // if cond. to check that check in date is not in the past
+                                if(temp.isBefore(today)) {
+                                    System.out.println("Error: Please enter a valid date");
+                                    continue;
+                                }
+                                else
+                                    inDate = temp;
+
+                                System.out.print("Enter Check-Out Date (YYYY-MM-DD): ");
+                                outDate = LocalDate.parse(sc.nextLine());
+                                if(!(inDate.isBefore(outDate))) {
+                                    System.out.println("Error: Check-in date must be before Check-out date");
+                                    continue;
+                                }
+                                break;
+                            } catch (Exception e) {
+                                System.out.println("Error: Please input date in the correct format");
+                            }
+                        }
+                        // Auto-generate ID
+                        int newId = HotelDatabase.getAllReservations().size() + 1;
+
+                        Reservation newRes = new Reservation(newId, this, selectRoom, inDate, outDate);
+                        HotelDatabase.addReservation(newRes);
+                        newRes.getRoom().setAvailable(false);
+
+                        System.out.println("Reservation successful! Your ID is: " + newId);
+
+                        System.out.println("Base Cost: " + newRes.calculateTotalCost());
+                        if(selectRoom.getAmenities() != null)
+                        {
+                            double totalPrice = newRes.calculateTotalCost();
+                            for (Amenity a : selectRoom.getAmenities()) {
+                                System.out.println("+ " + a.getName() + ": " + a.getAdditionalCost());
+                            }
+
+                            System.out.println("---------------------------");
+                            System.out.println("Total Cost: " + totalPrice);
+                        }
+                        break;
+                    } else {
+                        System.out.println("Room is not available or does not exist.");
+                        break;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Error: Invalid input, please input a valid room number");
+                }
+                break;
+
+                case "3":
+                    System.out.println("\n---View My Reservations---");
+                    boolean hasRes = false;
+                    for(Reservation res: HotelDatabase.getAllReservations()){
+                        if(res.getGuest().equals(this))
+                        {
+                            System.out.println(res);
+                            hasRes = true;
+                        }
+                    }
+                    if(!hasRes) {
+                        System.out.println("You currently have no reservations\n");
+                        break;
+                    }
+                    break;
+
+                case "4":
+                    System.out.println("\n---Cancel Reservation---");
+                    System.out.println("0.Go back\nEnter Reservation ID");
+                    int cancelId = -1;
+                    while(cancelId == -1) {
+                        try {
+                            cancelId = Integer.parseInt(sc.nextLine());
+                            if(cancelId == 0)
+                                break;
+                        } catch (NumberFormatException e){
+                                System.out.println("Please enter a valid reservation ID");
+                            } catch (Exception e){
+                                System.out.println("Unexpected error");
+                            }
+                        }
+                    Reservation cancelRes = HotelDatabase.findReservationById(cancelId);
+                    if(cancelRes == null)
+                    {
+                        System.out.println("Reservation does not exist");
+                        break;
+                    }
+                    if(!cancelRes.getGuest().equals(this))
+                        System.out.println("Access denied: You can only cancel your reservations ");
+                    //only pending and confirmed reservations can be cancelled
+                    if(!((cancelRes.getStatus() == ReservationStatus.PENDING) ||
+                            (cancelRes.getStatus() == ReservationStatus.CONFIRMED))){
+                        System.out.println("Reservation is not eligible for cancellation");
+                    }
+                    //if the guest is the one cancelling and the past condition
+                    if(cancelRes.getGuest().equals(this) && ((cancelRes.getStatus() == ReservationStatus.PENDING) ||
+                            (cancelRes.getStatus() == ReservationStatus.CONFIRMED)) ){
+                        cancelRes.setStatus(ReservationStatus.CANCELLED);
+                        cancelRes.getRoom().setAvailable(true);
+                        System.out.println("Reservation " + cancelId + " has been cancelled.");
+                    }
+                    else {
+                        System.out.println("Reservation cannot be cancelled");
+                    }
+                    break;
+
+                case "5":
+                    System.out.println("\n---Check-out---");
+                    System.out.print("0.Go back\nEnter Reservation ID to check out: ");
+                    int outId = -1;
+                    while(outId == -1){
+                    try {
+                        outId = Integer.parseInt(sc.nextLine());
+                        if(outId == 0)
+                            break;
+                    } catch (NumberFormatException e){
+                        System.out.println("Please enter a valid reservation ID");
+                    } catch (Exception e){
+                        System.out.println("Unexpected error");
+                    }
+                }
+
+                    Reservation resToOut = HotelDatabase.findReservationById(outId);
+
+                    if (resToOut != null && resToOut.getGuest().equals(this)) {
+                        double cost = resToOut.calculateTotalCost();
+                        if(resToOut.getRoom().getAmenities() != null)
+                        {
+                            for (Amenity a : resToOut.getRoom().getAmenities()) {
+                                cost += a.getAdditionalCost();
+                            }
+                        }
+                        System.out.println("Your total is: " + cost);
+
+                        PaymentMethod chosenMethod = null;
+
+                        while (chosenMethod == null) {
+                            System.out.println("\n--- Choose Payment Method ---");
+                            System.out.println("1. Cash");
+                            System.out.println("2. Credit Card");
+                            System.out.println("3. Online");
+                            System.out.print("Selection: ");
+
+                            String c = sc.nextLine();
+
+                            switch (c) {
+                                case "1":
+                                    chosenMethod = PaymentMethod.CASH;
+                                    break;
+                                case "2":
+                                    chosenMethod = PaymentMethod.CREDIT_CARD;
+                                    break;
+                                case "3":
+                                    chosenMethod = PaymentMethod.ONLINE;
+                                    break;
+                                default:
+                                    System.out.println("Invalid selection, please enter 1, 2, or 3");
+                                    break;
+                            }
+                        }
+                        try {
+
+                            if (this.pay(cost)) {
+                                resToOut.setStatus(ReservationStatus.CHECKED_OUT);
+                                resToOut.getRoom().setAvailable(true);
+                                int invoiceNo = HotelDatabase.getAllInvoices().size() + 1 ;
+                                Invoice inv = new Invoice(invoiceNo, resToOut);
+                                inv.processPayment(chosenMethod);
+                                inv.printInvoice();
+                            }
+                        } catch (Exception e) {
+                            if((e instanceof InsufficientBalanceException) ||
+                                    (e instanceof InvalidAmountException)) {
+                                System.out.println("Payment failed: " + e.getMessage());
+                            }
+                            else {System.out.println("Unexpected Error");}
+                        }
+                    } else {
+                        System.out.println("Invalid Reservation ID.");
+                    }
+                    break;
+
+                case "6":
+                    System.out.println("Logging out...");
+                    loggedIn = false;
+                    break;
+
+                default:
+                    System.out.println("Invalid choice, try again");
+            }
+
+        }
     }
 
 }
